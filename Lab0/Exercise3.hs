e3545196cec5918696a8b1242e45ab58d63afbd9
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-09-07 10:31:57 +0200

    finished exercise

--- Lab0/Exercise3.hs
+++ Lab0/Exercise3.hs
@@ -1,12 +1,13 @@
 
 import Data.List
 
+-- Time Spent: 90 Minutes, due to lack of syntax knowledge and compiler issues
+
 data Shape = NoTriangle | Equilateral
      | Isosceles  | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape 
 checkForDuplicates :: (Eq a ) => [a] -> Bool
--- removeElementFromList:: a -> [a] -> [a]
 
 
 checkForDuplicates [] = False 
@@ -16,21 +17,18 @@ checkForDuplicates (x:x':xs)
      | x /= x' = checkForDuplicates(x':xs)
      | otherwise = False
 
--- removeElementFromList _ [] = []
--- removeElementFromList a x:xs
---      | a == x = [xs]
---      | otherwise [a:xs]
 
--- isRectangular :: Integer -> Integer -> Integer -> Bool
--- isRectangular a b c = 
---      let hypothenuse = maximum[a,b,c]
---      let residuals = 
+isRectangular :: Integer -> Integer -> Integer -> Bool
+isRectangular a b c = 
+     filter (/= maximum[a,b,c]) [a,b,c]!!0^2
+     + filter (/= maximum[a,b,c]) [a,b,c]!!1^2
+     == maximum[a,b,c]^2
 
 triangle a b c 
     | any (<= 0) [a,b,c] = NoTriangle
     | a + b <= c || b + c <= a || a + c <= b = NoTriangle
     | all ((==) a) [b,c] = Equilateral
     | checkForDuplicates [a,b,c]  = Isosceles
-  --  | (\x y z <- (maximum[x,y,z] ) a b c) = Rectangular
+    | isRectangular a b c = Rectangular
     | otherwise = Other 
 
