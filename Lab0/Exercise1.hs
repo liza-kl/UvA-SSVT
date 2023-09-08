76c60d237ca06a396ab1b343d5f8edb04d719671
Denis Thiessen <mail@denisthiessen.de>
2023-09-08 13:00:16 +0200

    Added proof by induction to Exercise 1

--- Lab0/Exercise1.hs
+++ Lab0/Exercise1.hs
@@ -10,6 +10,32 @@ Otherwise,the difference in float number precision would falsify the equality.
 Mathematically, we could prove equality by induction. 
 -}
 
+{-
+    Proof by induction for each of the given statements.
+    1) 
+        I) n = 1 -> 1 (1 + 1)(2*1 + 1) / 6 = 1 -> Sum of 1 square is also one -> True
+        II) We assume that the statement is true for all natural numbers.
+        III) Proof it is still correct for n+1
+            -> n (n + 1) (2n + 1) / 6 + (n + 1)^2
+            -> (n (n + 1) (2n + 1) + 6*(n + 1)^2) / 6
+            -> ((n+1) (2n^2 + n) + 6*(n + 1)^2) / 6
+            -> (2n^3 + 9n^2 + 13n + 6) / 6
+            -> (n + 1) (2n^2 + 7n + 6) / 6
+            -> (n + 1) ((n + 2) (2n + 3)) / 6
+            -> (n + 1) ((n + 1) + 1) (2*(n + 1) + 1) / 6
+            -> Statement is true. 
+    2) 
+        I) n = 1 -> (1(1 + 1)/2) = 2/2 = 1 -> Sum of 1 cubed is also one -> True
+        II) We assume that the statement is true for all natural numbers.
+        III) Proof it is still correct for n+1
+            -> (n(n + 1) / 2)^2 + (n + 1)^3
+            -> ((n^4 + 2n^3 + n^2) / 4) + (4*(n + 1)^3 / 4)
+            -> ((n^4 + 2n^3 + n^2) / 4) + (4n^3 + 12n^2 + 12n + 4) / 4
+            -> (n^4 + 6n^3 + 13n^2 + 12n + 4) / 4
+            -> (n + 1)^2 (n + 2)^2 / 4
+            -> ((n+1) ((n+1) + 1) / 2)^2
+-}
+
 -- first equation
 sumOfNSquares :: Integer -> Integer
 sumOfNSquares' :: Integer -> Integer
