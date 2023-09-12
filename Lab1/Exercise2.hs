81b957457314ec9953315940e4493317bfe080d4
D45Hub <denis.thiessen@gmx.de>
2023-09-12 17:01:37 +0200

    Added base file of exercise 2

--- Lab1/Exercise2.hs
+++ Lab1/Exercise2.hs
@@ -2,5 +2,23 @@ import Data.List
 import System.Random
 import Test.QuickCheck
 
-subsequences :: [a] -> [[a]]
-subsequences [] = [[]]
+
+genSubsequences:: [a] -> [[a]]
+genSubsequences [] = [[]]
+genSubsequences (x:xs) = genSubsequences xs ++ map (x :) (genSubsequences xs)
+
+checkBaseDef:: Integer -> Bool
+checkBaseDef n = length( genSubsequences [1..n]) == 2^n
+
+-- Randomization of the input for the QuickCheck test
+genPositiveIntegersOverOne:: Gen Integer
+genPositiveIntegersOverOne = abs <$> (arbitrary :: Gen Integer) `suchThat` (>1)
+
+-- Output with QuickCheck tests
+main :: IO Result
+main = do
+        quickCheckResult $ forAll genPositiveIntegersOverOne checkBaseDef
+
+-- TODO WEITER AUSFÜHREN... U GOT DAT... :)
+-- Ist wegen 2er Potenzen und Rekursion absolut kacke. Deswegen teste ich da nur part of that specification, weil mein Rechner sonst bumm macht...
+
