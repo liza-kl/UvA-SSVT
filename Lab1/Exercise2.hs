2e3c8be2efe7ee831c632ba8b7a51f44a177d5cf
D45Hub <denis.thiessen@gmx.de>
2023-09-13 15:35:00 +0200

    Added exercise 2 comments...

--- Lab1/Exercise2.hs
+++ Lab1/Exercise2.hs
@@ -2,6 +2,22 @@ import Data.List
 import System.Random
 import Test.QuickCheck
 
+-- Time Spent ~ 1h20 
+-- Due to conceptualizing and Haskell issues...
+
+{- 
+
+        Proof by Induction that if A is finite set with |A| = n then |P(A)| = 2^n
+
+        Base case (n = 0): Empty set has 0 members and P(∅) = {∅} -> 1 Member -> Holds true since 2^0 also equals 1.
+
+        I.H. show for (n+1) -> |P(B)| = 2^(n+1).
+        P(B) contains all of the elements of P(A) as well as the (n+1)th element.
+        Since we need to unify every pre-existing element with the (n+1)th element the have to double the amount of elements.
+        Thus the amount of elements for |P(B)| = 2 * 2^n = 2^(n+1) 
+        -> Hypothesis holds true. □
+
+-}
 
 genSubsequences:: [a] -> [[a]]
 genSubsequences [] = [[]]
@@ -19,6 +35,14 @@ main :: IO Result
 main = do
         quickCheckResult $ forAll genPositiveIntegersOverOne checkBaseDef
 
--- TODO WEITER AUSFÜHREN... U GOT DAT... :)
--- Ist wegen 2er Potenzen und Rekursion absolut kacke. Deswegen teste ich da nur part of that specification, weil mein Rechner sonst bumm macht...
+{-
 
+        The property is hard to test due to an exponentially rising amount of subsequences.
+        So we have a high amount of subsequence elements even for smaller lists.
+        Since n can be any positive integer larger than 1 we naturally with ever increasing subsequence sizes, the computer reaches a processing limit.
+        Even if this limitation wouldn't exist then we would run into an overflow issue due to the corresponding data types.
+
+        I am not really testing a mathematical fact but rather just a small set where we can actually calculate the subsequences for.
+        We can't be completely sure using this method that this is completely correct, just the part of the specification which we could account for.
+        
+-}
