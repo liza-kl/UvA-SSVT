2db560480cdf798322aad0bf3a6bf12e07e899a7
D45Hub <denis.thiessen@gmx.de>
2023-09-14 13:26:37 +0200

    Made exercise 8 better.

--- Lab1/Exercise8.hs
+++ Lab1/Exercise8.hs
@@ -1,28 +1,27 @@
 import Data.List 
 
---- Exercise 8
+-- Exercise 8
+-- Time needed 2,5h due to trying to optimize list generation and other issues related to that.
 
 listOfPrimes:: [Integer]
 listOfPrimes = filter isPrime [2..]
 
-getPrimes:: Int -> [Integer]
-getPrimes amount = take amount listOfPrimes
+shrinkPrimes:: [a] -> [[a]] -> [[a]]
+shrinkPrimes [] _ = []
+shrinkPrimes xs acc = sublist : shrinkPrimes remaining (acc ++ [sublist])
+  where
+    sublist = take (length acc + 1) xs
+    remaining = xs ++ drop (length acc + 1) xs
 
-
-getPrimesList:: [[Integer]]
-getPrimesList = getPrimesList' 0
-
--- TODO IMPROVE THIS...
-getPrimesList':: Int -> [[Integer]]
-getPrimesList' idx = getPrimes idx : getPrimesList' (idx + 1)
-
-comparePrimesList:: [Integer] -> Bool
-comparePrimesList testedPrimes = testedPrimes == getPrimes (length testedPrimes)
+getShrinkingPrimes:: [[Integer]]
+getShrinkingPrimes = shrinkPrimes listOfPrimes []
 
 multiplyAdd1 :: [Integer] -> Integer
 multiplyAdd1 list1 = product list1 + 1
 
 isPrime :: Integer -> Bool
+-- Somehow it gets stuck on certain prime numbers...
+-- isPrime 7858321551080267055879091 = False
 isPrime n = all (\ x -> rem n x /= 0) xs
     where xs = takeWhile (\ y -> y^2 <= n) [2..]
 
@@ -30,4 +29,4 @@ mapListStructure:: [Integer] -> ([Integer], Integer)
 mapListStructure primes = (primes, multiplyAdd1 primes)
 
 counterexamples :: [([Integer], Integer)]
-counterexamples = filter (\(subs, p) -> not(isPrime p)) ( map mapListStructure getPrimesList)
+counterexamples = filter (\(subs, p) -> not(isPrime p)) ( map mapListStructure getShrinkingPrimes)
