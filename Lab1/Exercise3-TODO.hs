0cebad9b18be6d2e0a4f2881f37c73020d39a626
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-09-14 16:33:56 +0200

    added some notes to 3

--- Lab1/Exercise3-TODO.hs
+++ Lab1/Exercise3-TODO.hs
@@ -1,1 +1,26 @@
 {-- TODO --}
+
+import Data.List
+import Data.Char
+import Data.Maybe
+import Test.QuickCheck
+
+
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+forall :: [a] -> (a -> Bool) -> Bool
+forall = flip all
+
+stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
+stronger xs p q = forall xs (\ x -> p x --> q x)
+weaker   xs p q = stronger xs q p
+
+{-- 
+Rank the 4 expressions, not only bits
+
+(\ x -> even x && x > 3) or even
+(\ x -> even x || x > 3) or even
+(\ x -> (even x && x > 3) || even x) or even
+even or (\ x -> (even x && x > 3) || even x)
+--}
