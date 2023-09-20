e3ef58661fc6f3dc5965b26ba5807eca4fbc8195
D45Hub <denis.thiessen@gmx.de>
2023-09-20 17:01:26 +0200

    Added base for Form Generator

--- Lab1/Exercise7.hs
+++ Lab1/Exercise7.hs
@@ -1,6 +1,8 @@
 
 module Exercise7 where
 import SetOrd 
+import Test.QuickCheck
+import System.Random
 
 type Name = Int
 
@@ -19,6 +21,23 @@ sub f@(Cnj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
 sub f@(Dsj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2) 
 sub f@(Impl f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2) 
 sub f@(Equiv f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
+
+
+
+instance Arbitrary Form where
+    arbitrary = do
+        f1 <- arbitrary
+        f2 <- arbitrary
+        elements [ Prop f1
+                , Prop (randomRIO (1,10)) -- TODO SOMEHOW GENERATE A RANDOM NUMBER IN THERE
+                 , Neg (Prop f1)
+                 , Cnj [Prop f1, Prop f2]
+                 , Dsj [Prop f1, Prop f2]
+                 , Impl (Prop f1) (Prop f2)
+                 , Equiv (Prop f1) (Prop f2)
+                 ]
+
+
 
 {-- nsub :: Form -> Int 
 
