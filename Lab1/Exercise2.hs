05491a3ea9d95027cb952d633506202978477cd7
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-09-20 15:06:03 +0200

    Exercise 2 added a comment

--- Lab1/Exercise2.hs
+++ Lab1/Exercise2.hs
@@ -1,6 +1,7 @@
 module Exercise2 where
 
-import Data.List
+import qualified Data.List (subsequences)
+
 import System.Random
 import Test.QuickCheck
 
@@ -21,12 +22,12 @@ import Test.QuickCheck
 
 -}
 
-genSubsequences:: [a] -> [[a]]
-genSubsequences [] = [[]]
-genSubsequences (x:xs) = genSubsequences xs ++ map (x :) (genSubsequences xs)
+subsequences:: [a] -> [[a]]
+subsequences [] = [[]]
+subsequences (x:xs) = subsequences xs ++ map (x :) (subsequences xs)
 
 prop_checkBaseDef:: Integer -> Bool
-prop_checkBaseDef n = length( genSubsequences [1..n]) == 2^n
+prop_checkBaseDef n = length( subsequences [1..n]) == 2^n
 
 -- Randomization of the input for the QuickCheck test
 genPositiveIntegersOverOne:: Gen Integer
@@ -37,14 +38,16 @@ main :: IO Result
 main = do
         quickCheckResult $ forAll genPositiveIntegersOverOne prop_checkBaseDef
 
-{-
+{--
 
-        The property is hard to test due to an exponentially rising amount of subsequences.
-        So we have a high amount of subsequence elements even for smaller lists.
+        1. The property is computationally hard to test due to an exponentially rising amount of subsequences (exponential time complexity
+        and the domain is +∞).
+
+        2. So we have a high amount of subsequence elements even for smaller lists.
         Since n can be any positive integer larger than 1 we naturally with ever increasing subsequence sizes, the computer reaches a processing limit.
         Even if this limitation wouldn't exist then we would run into an overflow issue due to the corresponding data types.
 
         I am not really testing a mathematical fact but rather just a small set where we can actually calculate the subsequences for.
         We can't be completely sure using this method that this is completely correct, just the part of the specification which we could account for.
         
--}
+--}
