797e72ec790eddfbc4b286ce6f44b05832f174fc
D45Hub <denis.thiessen@gmx.de>
2023-09-20 17:08:13 +0200

    Potential fix for generator

--- Lab1/Exercise7.hs
+++ Lab1/Exercise7.hs
@@ -3,6 +3,8 @@ module Exercise7 where
 import SetOrd 
 import Test.QuickCheck
 import System.Random
+import Lecture2
+import System.IO.Unsafe
 
 type Name = Int
 
@@ -22,14 +24,15 @@ sub f@(Dsj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
 sub f@(Impl f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2) 
 sub f@(Equiv f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
 
-
+genRandomInteger:: Int -> Int
+genRandomInteger num = unsafePerformIO(Lecture2.getRandomInt num)
 
 instance Arbitrary Form where
     arbitrary = do
         f1 <- arbitrary
         f2 <- arbitrary
         elements [ Prop f1
-                , Prop (randomRIO (1,10)) -- TODO SOMEHOW GENERATE A RANDOM NUMBER IN THERE
+                , Prop (genRandomInteger 5)-- TODO SOMEHOW GENERATE A RANDOM NUMBER IN THERE
                  , Neg (Prop f1)
                  , Cnj [Prop f1, Prop f2]
                  , Dsj [Prop f1, Prop f2]
