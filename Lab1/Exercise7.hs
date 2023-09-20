d305b7c96c0d32a8d17cfd03ef86bd9b354dfc18
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-09-20 21:40:08 +0200

    used choose instead of unsafeIO

--- Lab1/Exercise7.hs
+++ Lab1/Exercise7.hs
@@ -1,38 +1,37 @@
 
 module Exercise7 where
-import SetOrd 
+import SetOrd
 import Test.QuickCheck
 import System.Random
 import Lecture2
 import System.IO.Unsafe
+import Test.QuickCheck (Arbitrary(arbitrary))
 
 type Name = Int
 
-data Form = Prop Name 
-    | Neg  Form 
-    | Cnj [Form] 
-    | Dsj [Form] 
-    | Impl Form Form 
-    | Equiv Form Form 
+data Form = Prop Name
+    | Neg  Form
+    | Cnj [Form]
+    | Dsj [Form]
+    | Impl Form Form
+    | Equiv Form Form
     deriving (Eq,Ord, Show)
 
-sub :: Form -> Set Form 
-sub (Prop x) = Set [Prop x] 
-sub (Neg f) = unionSet (Set [Neg f]) (sub f) 
-sub f@(Cnj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2) 
-sub f@(Dsj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2) 
-sub f@(Impl f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2) 
+sub :: Form -> Set Form
+sub (Prop x) = Set [Prop x]
+sub (Neg f) = unionSet (Set [Neg f]) (sub f)
+sub f@(Cnj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
+sub f@(Dsj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
+sub f@(Impl f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
 sub f@(Equiv f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
-
-genRandomInteger:: Int -> Int
-genRandomInteger num = unsafePerformIO(Lecture2.getRandomInt num)
 
 instance Arbitrary Form where
     arbitrary = do
         f1 <- arbitrary
         f2 <- arbitrary
+        rndNum <- choose(1,15)
         elements [ Prop f1
-                , Prop (genRandomInteger 5)-- TODO SOMEHOW GENERATE A RANDOM NUMBER IN THERE
+                , Prop rndNum 
                  , Neg (Prop f1)
                  , Cnj [Prop f1, Prop f2]
                  , Dsj [Prop f1, Prop f2]
