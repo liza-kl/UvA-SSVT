99bc9394c819f20ee0ba810288af9fc4a0cc5949
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-09-28 14:11:05 +0200

    draft exercise 5

--- Lab2/Exercise5.hs
+++ Lab2/Exercise5.hs
@@ -1,9 +1,22 @@
 module Exercise5 where
 
-import LTS 
+import LTS
 {-- Haskell program, description of each bug, indication of time spent. --}
 
 -- Taking the door 1 implementation as a basis for the IOLTS 
+-- We need to implement the output function in order to check other stuff
+-- Definition found in presentation slides 
+out :: IOLTS -> [State] -> [Label]
+-- case for when the state list is empty, where we return the delta LTS datatype
+out  (states,inputs,outputs,transitions,state) [] = [delta]
+out (_, _, _, transitions, _) states =
+    map (\s -> nextTransitions' transitions s) states
+
+
+
+
+-- 
+
 
 {-- 
 doorImpl1 :: State -> Label -> (State, Label)
@@ -16,7 +29,7 @@ doorImpl1 _ _ = error "Invalid command and/or state!"
 
 {-- The correctness of the implementation can be tests with ioco against the other door implementations --}
 
-correctDoorModel :: IOLTS 
+correctDoorModel :: IOLTS
 correctDoorModel = createIOLTS [
                 (0,"?close", 1),(0,"!closed",1),
                 (1,"?open", 0),(1,"!opened",0),
@@ -26,7 +39,6 @@ correctDoorModel = createIOLTS [
 -- testLTSAgainstSUT :: IOLTS -> (State -> Label -> (State, Label)) -> Bool
 
 -- In the end you need to print smt like doorImplX ioco correctDoorModel
-main :: IO () 
-main = 
-    do 
-        print "a door"
+main :: IO ()
+main =
+    print "a door"
