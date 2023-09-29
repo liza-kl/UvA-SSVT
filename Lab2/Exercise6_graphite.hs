013348d04192cb9c436ec046c0adb0d657291ffb
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-09-29 19:39:09 +0200

    Added a visualization library

---
+++ Lab2/Exercise6_graphite.hs
@@ -0,0 +1,19 @@
+
+
+module Exercise6Graphite where
+import GHC.Generics (Generic)
+import Data.Hashable
+
+import Data.Graph.DGraph
+import Data.Graph.UGraph
+import Data.Graph.Visualize
+import Data.Graph.Types
+import LTS
+
+
+visualizeLTS' :: [LabeledTransition] -> DGraph Integer String
+visualizeLTS' transitions = fromArcsList (map (\(pre, trans, post) -> Arc pre post trans) transitions)
+
+visualizeLTS (_,_,_,transitions,_)= visualizeLTS' transitions
+
+main = plotDGraphPng (visualizeLTS tretmanR2) "./tmp/tretmanR2"
