3db7eed466fbd81e85bdfb2c8d7bf1daf90b0b9f
D45Hub <denis.thiessen@gmx.de>
2023-09-29 17:03:17 +0200

    Added crude version of door checks

--- Lab2/Exercise5.hs
+++ Lab2/Exercise5.hs
@@ -1,5 +1,6 @@
 module Exercise5 where
 import Data.List
+import Data.Maybe
 import Exercise4 
 import LTS
 import Debug.Trace (trace)
@@ -46,12 +47,67 @@ doorImpl2ToIOLTS = createIOLTS [
                 (2, "?unlock", 1), (2,"!unlocked",1)]
 
 
--- doorImpl3ToIOLTS :: IOLTS
--- doorImpl3ToIOLTS = createIOLTS [
---                 (0,"?close", 1),(0,"!closed",1),
---                 (1,"?open", 0),(1,"!opened",0),
---                 (1, "?lock", 2), (1,"!locked",2),
---                 (2, "?unlock", 2), (2,"!unlocked",2)]
+doorImpl3ToIOLTS :: IOLTS
+doorImpl3ToIOLTS = createIOLTS [
+                (0,"?close", 1),(0,"!closed",1),
+                (1,"?open", 0),(1,"!opened",0),
+                (1, "?lock", 2), (1,"!locked",2),
+                (2, "?unlock", 2), (2,"!unlocked",2)]
+
+doorImpl4ToIOLTS :: IOLTS
+doorImpl4ToIOLTS = createIOLTS [
+                (0,"?close",1), (0, "!closed", 1),
+                (1, "?open", 0), (1,"!opened",0),
+                (1,"?unlock",2),(1,"!locked",2),
+                (2,"?lock",1),(2,"!unlocked",1)]
+
+doorImpl5ToIOLTS :: IOLTS
+doorImpl5ToIOLTS = createIOLTS[
+                    (0,"?close", 1),(0,"!closed",1),
+                    (1,"?open", 0),(1,"!opened",0),
+                    (1, "?lock", 2), (1,"!locked",2),
+                    (2, "?unlock", 1), (2,"!unlocked",1)]
+
+doorImpl6ToIOLTS :: IOLTS 
+doorImpl6ToIOLTS = createIOLTS[
+    (0, "?close", 1),(0,"!closed",1),
+    (1, "?open", 3),(1,"!opened",3),
+    (3, "?close", 4),(3,"!closed",4),
+    (4, "?open", 5),(4,"!opened",5),
+    (5, "?close", 6),(5,"!closed",6),
+    (6, "?open", 6),(6,"delta",6),
+    (1, "?lock", 2),(1,"!locked",2),
+    (4, "?lock", 2),(1,"!locked",2),
+    (6, "?lock", 2),(6,"!locked",2),
+    (2, "?unlock", 1),(2,"!unlocked",1)]
+
+doorImpl7ToIOLTS :: IOLTS
+doorImpl7ToIOLTS = createIOLTS [
+    (0,"?close", 1), (0,"!closed",1),
+    (1,"?open", 0), (1,"!opened",0),
+    (1, "?lock", 2), (1,"!locked",2),
+    (2, "?unlock", 3), (2,"!unlocked",3),
+    (4, "?close", 5), (4,"!closed",5),
+    (3, "?open", 4), (3,"!opened",4),
+    (3, "?lock", 2), (3,"!locked",2),
+	(5, "?open", 0), (5,"!opened",0),
+	(5, "?lock", 6), (5,"!locked",6)]
+
+doorImpl8ToIOLTS :: IOLTS
+doorImpl8ToIOLTS = createIOLTS [
+    (0,"?close", 1),(0,"!closed",1),
+    (1,"?open", 0),(1,"!opened",0),
+    (1, "?lock", 2), (1,"!locked",2),
+    (2, "?unlock", 3), (2,"!unlocked",3),
+    (4, "?close", 5), (4,"!closed",5),
+    (3, "?open", 4), (3,"!opened",4),
+    (3, "?lock", 2), (3,"!locked",2),
+    (5, "?open", 6), (5,"!opened",6),
+    (5, "?lock", 2), (5,"!locked",2),
+    (6, "?close", 7), (6,"!closed",7),
+    (7, "?lock", 2), (7,"!locked",2),
+    (7, "?close", 2), (7,"!closed",2)]
+
 
 {-- This function receives the model and the implementation
     First of all, another function should be created that will convert the implementation to an IOLTS
@@ -63,11 +119,26 @@ doorImpl2ToIOLTS = createIOLTS [
     Then, testLTSAgainstSUT will just call ioco with the two IOLTS's and return the result
 --}
 
+getDoorLTSs :: [IOLTS]
+getDoorLTSs = [doorImpl2ToIOLTS, doorImpl3ToIOLTS, doorImpl4ToIOLTS]
+
 testLTSAgainstSUT :: IOLTS -> (State -> Label -> (State, Label)) -> Bool
 testLTSAgainstSUT model impl = 
-    trace ("ioco: " ++ show (ioco doorImpl2ToIOLTS model)) $
-    ioco doorImpl2ToIOLTS model
+    trace ("ioco: " ++ show (ioco correctDoorModel model)) $
+    ioco correctDoorModel model
     -- ioco doorImpl3ToIOLTS model
+
+
+
+{--
+testLTSsAgainstSUT :: [IOLTS] -> (State -> Label -> (State, Label)) -> [Bool]
+testLTSsAgainstSUT models impl = map (`ioco` correctDoorModel) getDoorLTSs
+--}
+
+testLTSsAgainstSUT :: [IOLTS] -> (State -> Label -> (State, Label)) -> [(String,Bool)]
+testLTSsAgainstSUT models impl = map (\impl -> ("Door " ++ show ( ((fromMaybe 0 (elemIndex impl models)) + 2) ),impl `ioco` correctDoorModel)) getDoorLTSs
+--testLTSsAgainstSUT [] impl = []
+--testLTSsAgainstSUT (x:xs) impl = (testLTSAgainstSUT x impl) ++ (testLTSsAgainstSUT xs impl) 
 
 -- Define ioco Function to get the outputs.
 -- Definition of ioco is for all (s)traces of the model must confirm: out ( implementation after trace ) is a subset
@@ -85,14 +156,19 @@ ioco implementation model = and
 -- In the end you need to print smt like doorImplX ioco correctDoorModel
 main :: IO ()
 main = do
-    print (testLTSAgainstSUT correctDoorModel doorImpl2)
+    print (testLTSsAgainstSUT getDoorLTSs doorImpl1)
+--    print (testLTSAgainstSUT correctDoorModel doorImpl2)
 
 {-- Test Report
 
     - doorImpl1: correct
     - doorImpl2: when it closes, it goes to an opened state
     - doorImpl3: when it unlocks, it transitions to the wrong state (2)
-    - doorImpl4: when it locks, it transitions to the wrong state (1) && when it unlocks, it transitions to the wrong state (2) 
+    - doorImpl4: When you are in State (1) and "unlock" the door, it's locked and in state (2), when you are in state (2) and lock the door , its unlocked and in state (1) 
+    - doorImpl5: same as door 1?
+    - doorImpl6: After the trace ["closed","open"] door 6 can be in states [3,5,6] but the opened door only in 0
+    - doorImpl7:  ["closed","lock"] 
+
 
 
 
