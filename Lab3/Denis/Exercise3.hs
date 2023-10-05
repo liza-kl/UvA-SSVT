ced0f2f298569adbd6cc90216021b858ce7924b3
D45Hub <denis.thiessen@gmx.de>
2023-10-05 16:27:45 +0200

    Added some weird ideas to exercise 3

--- Lab3/Denis/Exercise3.hs
+++ Lab3/Denis/Exercise3.hs
@@ -1,1 +1,20 @@
 module Denis.Exercise3 where
+import Denis.Exercise2
+import Denis.Exercise1
+import MultiplicationTable
+import Data.List
+import Mutation
+import Test.QuickCheck
+import System.IO.Unsafe
+
+killsNoMutants:: Integer -> [([Integer] -> Gen [Integer])] -> ([Integer] -> Integer -> Bool) -> (Integer -> [Integer]) -> Bool
+killsNoMutants amount mutants property func = (countSurvivors amount mutants [property] func) == amount
+
+generateMutationLists:: Integer -> [([Integer] -> Gen [Integer])] -> [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> [[Bool]]
+generateMutationLists amount mutants [] func = []
+generateMutationLists amount mutants (x:xs) func = do
+                                                        return (generate (mutate' mutator properties func getRandomNumber))
+
+
+getRandomNumber:: Integer
+getRandomNumber = unsafePerformIO(generate (choose(1,1000)))
