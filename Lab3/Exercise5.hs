7c939d53039f2973053271699047a3d935bd798d
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-10-05 16:24:22 +0200

    :memo: Added basic comments to Exercise5

---
+++ Lab3/Exercise5.hs
@@ -0,0 +1,12 @@
+module Exercise5 where
+
+-- ## Task ##
+-- Implement function(s) that calculate
+-- the conjectures: properties that are equivalent, whose cases are subsets of other properties, etc
+
+-- ## Deliverables ##
+-- Implementations, documentation of approach, indication of time spent
+
+-- ## Documentation of approach ##
+
+-- ## Code Trash ##
