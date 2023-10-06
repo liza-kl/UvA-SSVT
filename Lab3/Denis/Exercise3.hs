4f5f54eae47eb5f147f9bdea82a498774fc190d1
D45Hub <denis.thiessen@gmx.de>
2023-10-06 13:37:08 +0200

    Added alternative concept ideas to Exercise 3

--- Lab3/Denis/Exercise3.hs
+++ Lab3/Denis/Exercise3.hs
@@ -7,6 +7,8 @@ import Mutation
 import Test.QuickCheck
 import System.IO.Unsafe
 
+
+-- TODO WRITE UP THIS SHITTY APPROACH BASED ON THE VERY FORMAL DEFINITION...
 killsNoMutants:: Integer -> [([Integer] -> Gen [Integer])] -> ([Integer] -> Integer -> Bool) -> (Integer -> [Integer]) -> Bool
 killsNoMutants amount mutants property func = (countSurvivors amount mutants [property] func) == amount
 
@@ -18,3 +20,26 @@ generateMutationLists amount mutants (x:xs) func = do
 
 getRandomNumber:: Integer
 getRandomNumber = unsafePerformIO(generate (choose(1,1000)))
+
+{- 
+
+    Alternative approach:
+    The minimal property subset consists of two given properties.
+
+    1) If a property kills no mutants, then it isn't in the MSP (minimum property subset)
+    2) If a property kills the same or a subset of the same mutants than another property, it is redundant, thus also not in the MPS.
+
+    Therefore we could structuralize it in the following way.
+        - We create a function which checks if for a given property of the initial property set, the mutate' function only returns "True" values. 
+          In that case, all mutants survived, no mutant was killed and property 1 holds. So we can remove that property from the given set.
+        - We then go through the rest of the properties and remove all properties whose mutation survival/kill result list is equal or a subset of another property's mutation list.
+          In this case we can also remove the property, which is equal or weaker to another property.
+
+    Then both properties hold, and the MPS is created from the initial property set.
+
+    Other concept:
+    We could apply a "search" or "optimization" algorithm approach and use one of the algorithms present there.
+    The goal of such a search algorithm would be to optimize for the fewest amount of properties, which still hold the same result from the mutate' function as with the original property set.
+    TODO Example of Search Algorithm in place??? But that could be a concept as well...
+
+-}
