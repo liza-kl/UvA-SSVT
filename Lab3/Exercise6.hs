5da9c2a30a9ad12b3c75bc31dfe71f12f7a96c6e
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-10-06 18:49:01 +0200

    added percentage to 6

--- Lab3/Exercise6.hs
+++ Lab3/Exercise6.hs
@@ -6,8 +6,9 @@ import Mutation
 import Exercise1
 import Test.QuickCheck
 import MultiplicationTable
+import Exercise4
 import System.IO.Unsafe
-import MultiplicationTable (prop_moduloIsZero)
+import MultiplicationTable (prop_moduloIsZero, prop_tenElements)
 
 -- ## Task ##
 -- Create a function that we pass the function under test, 
@@ -34,43 +35,49 @@ import MultiplicationTable (prop_moduloIsZero)
 getMutators = [shuffleList]
 getproperties = [prop_tenElements, prop_firstElementIsInput, prop_sumIsTriangleNumberTimesInput, prop_linear, prop_moduloIsZero]
 
-representSurvivorsAndKilled :: IO Integer
+representSurvivorsAndKilled :: IO Float
 representSurvivorsAndKilled = do
-    survivors <- generate $ countSurvivors 4000 [prop_linear] shuffleList multiplicationTable
-    return survivors
+    survivors <- generate $ countSurvivors 4000 [prop_tenElements] shuffleList multiplicationTable
+    let floatedSurvivors = fromInteger survivors :: Float
+    return floatedSurvivors
+
+representPercentageOfKilled :: IO Float
+representPercentageOfKilled = do
+    killPercentage <- calculateSurvivorPercentage 4000 [prop_tenElements] shuffleList multiplicationTable
+    return killPercentage
+
 
 
 
 -- Define a data structure for a row in the table
-data TableRow = TableRow { name :: String, result :: Integer }
+data TableRow = TableRow { name :: String, result':: String, result ::  Float }
 
 -- Create a list of sample data
 sampleData :: [TableRow]
 sampleData =
-    [ TableRow "Killed Mutants in Percentage" 35
-    , TableRow "Used Mutants" ()
-    , TableRow "Used Properties" ()
-    , TableRow "Survivors" (unsafePerformIO representSurvivorsAndKilled)
+    [ TableRow  "Killed Mutants in Percentage" "" (unsafePerformIO representPercentageOfKilled)
+    , TableRow "Survivors" "" (unsafePerformIO representSurvivorsAndKilled)
     ]
 
 -- Function to print the table
 printTable :: [TableRow] -> IO ()
 printTable rows = do
-    putStrLn mainHeader
     putStrLn header
     putStrLn separator
     mapM_ printRow rows
     where
-    mainHeader ="| REPORT                          |"
     header =    "| Function | Result  |            |"
     separator = "+----------+---------+------------+"
 
-    printRow row = putStrLn $ "| " ++ formatField (name row) ++ " | "
+    printRow row = putStrLn $ "| " ++ formatField (name row) ++ " | " ++
+                            "| " ++ formatField (result' row) ++ " | "
                             ++ formatField (show $ result row) ++ " | "
 
-    formatField field = let width = 20 in take width (field ++ repeat ' ')
+    formatField field = let width = 30 in take width (field ++ repeat ' ')
 
 main :: IO ()
 main = do
-    putStrLn "Table Example:"
+    putStrLn ""
+    putStrLn "REPORT:"
+    putStrLn ""
     printTable sampleData
