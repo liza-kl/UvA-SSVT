67e9523cce926049027b347394e8aeb0d1a9df7d
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-10-10 21:23:14 +0200

    Exercise 3

--- Lab4/Exercise3.hs
+++ Lab4/Exercise3.hs
@@ -1,1 +1,13 @@
 module Exercise3 where
+
+-- ## Deliverables 
+-- Haskell program, indication of time spent
+-- ## Indication of type spent
+-- 5 Minutes?! 
+
+type Rel a = [(a,a)]
+
+symClos :: Ord a => Rel a -> Rel a
+symClos [] = []
+symClos relation = concat [ [(x,y),(y,x)] | (x,y) <- relation] 
+
