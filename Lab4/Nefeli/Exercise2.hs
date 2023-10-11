9c12f458bee720c94e9ed5d8be2aed779e66d64f
nefelitav <nefeli.tavoulari123@gmail.com>
2023-10-11 15:39:55 +0200

    fix syntax errors

--- Lab4/Nefeli/Exercise2.hs
+++ Lab4/Nefeli/Exercise2.hs
@@ -9,16 +9,16 @@ setUnion :: Ord a => Set a -> Set a -> Set a
 setUnion set1 set2 = unionSet set1 set2
 
 -- A∪B=B∪A
-prop_unionCommulative :: Eq a => Set a -> Set a -> Bool
-prop_unionCommulative set1 set2 = unionSet set1 set2 == unionSet set2 set1 
+prop_unionCommulative :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_unionCommulative set1 set2 = property (setUnion set1 set2 == setUnion set2 set1) 
 
 -- (A∪B)∪C=A∪(B∪C)
-prop_unionAssociative :: Eq a => Set a -> Set a -> Set a -> Bool
-prop_unionAssociative set1 set2 set3 = unionSet ((unionSet set1 set2) set3) == unionSet (set1 unionSet (set2 set3))  
+prop_unionAssociative :: (Eq a, Ord a) => Set a -> Set a -> Set a -> Property
+prop_unionAssociative set1 set2 set3 = property (setUnion (setUnion set1 set2) set3 == setUnion set1 (setUnion set2 set3))
 
 -- A∪A=A
-prop_unionIdempotent :: Eq a => Set a -> Bool
-prop_unionIdempotent set1 = unionSet(set1 set1) == set1
+prop_unionIdempotent :: (Eq a, Ord a) => Set a -> Property
+prop_unionIdempotent set1 = property (setUnion set1 set1 == set1)
 
 -- A - B
 -- if A is empty, A - B is empty
@@ -35,14 +35,14 @@ setDifference (Set (x:xs)) set2
   | inSet x set2 = setDifference (Set xs) set2
   | otherwise = insertSet x (setDifference (Set xs) set2)
 
-prop_differenceEmptySet1 :: Eq a => Set a -> Set a -> Property
-prop_differenceEmptySet1 set1 set2 = setDifference set1 set2 == emptySet
+prop_differenceEmptySet1 :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_differenceEmptySet1 set1 set2 = property (setDifference set1 set2 == emptySet)
 
-prop_differenceEmptySet2 :: Eq a => Set a -> Set a -> Property
-prop_differenceEmptySet2 set1 set2 = setDifference set1 set2 == set1
+prop_differenceEmptySet2 :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_differenceEmptySet2 set1 set2 = property (setDifference set1 set2 == set1)
 
-prop_differenceSameSet :: Eq a => Set a -> Property
-prop_differenceSameSet set1 = setDifference set1 set1 == emptySet
+prop_differenceSameSet :: (Eq a, Ord a) => Set a -> Property
+prop_differenceSameSet set1 = property (setDifference set1 set1 == emptySet)
 
 -- A ∩ B
 -- if A or B is empty, A ∩ B is empty
@@ -58,13 +58,13 @@ setIntersection (Set (x:xs)) set2
   | otherwise = setIntersection (Set xs) set2
 
 -- A∩B=B∩A
-prop_intersectionCommulative :: Eq a => Set a -> Set a -> Property
-prop_intersectionCommulative set1 set2 = setIntersection set1 set2 == setIntersection set2 set1 
+prop_intersectionCommulative :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_intersectionCommulative set1 set2 = property (setIntersection set1 set2 == setIntersection set2 set1) 
 
 -- (A∩B)∩C=A∩(B∩C)
-prop_intersectionAssociative :: Eq a => Set a -> Set a -> Set a -> Property
-prop_intersectionAssociative set1 set2 set3 = setIntersection ((setIntersection set1 set2) set3) == setIntersection (set1 setIntersection (set2 set3))  
+prop_intersectionAssociative :: (Eq a, Ord a) => Set a -> Set a -> Set a -> Property
+prop_intersectionAssociative set1 set2 set3 = property (setIntersection (setIntersection set1 set2) set3 == setIntersection set1 (setIntersection set2 set3))
 
 -- A∩(B∩C)=(A∩B)∪(A∩C)
-prop_intersectionDistributive :: Eq a => Set a -> Set a -> Set a -> Property
-prop_intersectionAssociative set1 set2 set3 = setIntersection (set1 setIntersection (set2 set3)) == setUnion(setIntersection(set1 set2) setIntersection(set1 set3))
+prop_intersectionDistributive :: (Eq a, Ord a) => Set a -> Set a -> Set a -> Property
+prop_intersectionDistributive set1 set2 set3 = property (setIntersection set1 (setIntersection set2 set3) == setUnion (setIntersection set1 set2) (setIntersection set1 set3))
