6f46fd675630c4c5bd2752a0fe8c980c9a81dedb
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-10-11 10:03:19 +0200

    Ex 7 short answer; counter example

---
+++ Lab4/Lisa/Exercise7.hs
@@ -0,0 +1,11 @@
+module Exercise7 where
+    -- Relation:[ (1, 2), (2, 3), (3, 4)]
+    -- Transitive Closure [(1,2),(2,3),(3,4),(1,3),(2,4),(1,4)]
+    -- Symmetric Closure:  [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3)]
+
+    -- Symmetric closure of transitive closure: [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3),(1,3),(3,1),(2,4),(4,2),(1,4),(4,1)]
+    -- Transitive closure of symmetric closure: [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3),(1,1),(1,3),(2,2),(2,4),(3,1),(3,3),(4,2),(4,4),(1,4),(4,1)]
+
+    -- They are not the same? 
+    
+    
