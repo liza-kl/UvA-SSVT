9bbf7394c041a70c8f5430db91c0e299cd099382
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-10-12 14:39:33 +0200

    props for Ex2

--- Lab4/Exercise2.hs
+++ Lab4/Exercise2.hs
@@ -25,6 +25,9 @@ prop_unionAssociative set1 set2 set3 = property (setUnion (setUnion set1 set2) s
 prop_unionIdempotent :: (Eq a, Ord a) => Set a -> Property
 prop_unionIdempotent set1 = property (setUnion set1 set1 == set1)
 
+prop_intersectionIdempotent :: (Eq a, Ord a) => Set a -> Bool
+prop_intersectionIdempotent set1 = setIntersection set1 set1 == set1
+
 -- A - B
 -- if A is empty, A - B is empty
 -- if B is empty, A - B is A
@@ -96,16 +99,40 @@ setToList (Set (x:xs)) = x : setToList (Set xs)
 setLength:: Set a -> Int
 setLength set = length (setToList set)
 
+prop_setReflexivity ::  (Eq a, Ord a) => Set a -> Bool
+prop_Transitivity ::  (Eq a, Ord a) => Set a -> Bool
 
 
 -- ## Testing 
+-- Properties were chosen based on the Theorem 4.16 from the Haskell Road to Logic Book.
+-- We can derive from that that if all of our functions are correctly implemented then the
+-- laws of idempotence, commutativity, associativity, distributivity, De Morgan's Laws should result in true.
+--
+-- Due to the fact that we don't test our set generators in Ex. 1 we also include some "basic" properties which 
+-- should validate that our input sets are sorted, and have unique elements. 
+-- According to Cantor's Comprehension Principle (pg. 125) & Theorem 4.1 in the Haskell Road Logic Book we need to check for 
+-- reflexivity, antisymmetry, transitivity (this makes a set valid)
+
+--
+-- Writing all the stuff down which was proven by many intelligent people before us would be in our sense kind of pointless,
+-- so we just say that we can prove it with Venn Diagramms or Truth Tables. 
+-- Although the properties seem kind of complete, we are limited to the fact, that we can't test infinite sets.
+-- Maybe we can prove by induction that if it holds for n, it holds for n+1 ?
+
+-- Further references for the proofs are: XYZ TODO: Add references.
+
 main = do
+    print "Testing Idempotence. Can be proven with Venn Diagramms or Truth Tables"
+    print "Testing prop_unionIdempotent"
+    quickCheckResult $ forAll generateSets' prop_unionIdempotent
+    print "Testing prop_intersectionIdempotent"
+    quickCheckResult $ forAll generateSets' prop_intersectionIdempotent
+
     print "Testing prop_unionCommulative"
     quickCheckResult $ forAll generateSets' prop_unionCommulative
     print "Testing prop_unionAssociative"
     quickCheckResult $ forAll generateSets' prop_unionAssociative
-    print "Testing prop_unionIdempotent"
-    quickCheckResult $ forAll generateSets' prop_unionIdempotent
+
     print "Testing prop_differenceSameSet"
     quickCheckResult $ forAll generateSets' prop_differenceSameSet
     print "Testing setDifference"
@@ -126,6 +153,13 @@ main = do
     quickCheckResult $ forAll generateSets' prop_absorptionLaw
     print "Testing prop_intersectionInDifference"
     quickCheckResult $ forAll generateSets' prop_intersectionInDifference
+    print "Testing intersection over empty set"
+    print "Testing union over empty set"
+    print "Testing union over empty set"
+
+
+
+
 
 
 
