1309a9161735e8c64de3bf62802f2ff7dbef92ee
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-10-12 15:48:35 +0200

    :memo: To Ex 7

--- Lab4/Exercise7.hs
+++ Lab4/Exercise7.hs
@@ -1,1 +1,33 @@
 module Exercise7 where
+
+    --TODO Elaborate more.
+    -- Relation:[ (1, 2), (2, 3), (3, 4)]
+    -- Transitive Closure [(1,2),(2,3),(3,4),(1,3),(2,4),(1,4)]
+    -- Symmetric Closure:  [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3)]
+
+    -- Symmetric closure of transitive closure: [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3),(1,3),(3,1),(2,4),(4,2),(1,4),(4,1)]
+    -- Transitive closure of symmetric closure: [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3),(1,1),(1,3),(2,2),(2,4),(3,1),(3,3),(4,2),(4,4),(1,4),(4,1)]
+
+    -- They are not the same? 
+    
+    -- Haha, it depends on the domain which you are using it. 
+
+    -- It depends on the set you are using.
+
+    -- e.g. For the relation: [(1,2),(2,3)] they are indeed the same. 
+    -- symclos [(1,2),(2,1),(2,3),(3,2)]
+    -- trclos [(1,2),(2,3),(1,3)]
+    -- Symmetric closure of transitive closure: [(1,2),(2,1),(2,3),(3,2),(1,3),(3,1)]
+    -- Transitive closure of symmetric closure: [(1,2),(2,1),(2,3),(3,2),(1,1),(1,3),(2,2),(3,1),(3,3)]
+    -- if a relation is reflexive it can be 
+
+-- transitive closure has identiy in symmetric closure
+-- Symmetric Closure and Transitive closure can be the same if you have an empty set or only identity sets inside
+-- of it 
+
+-- Maybe reason with some commuative law etc.
+
+    -- You can prob remove the duplicates of the transitive closure 
+    -- as this is not expected so you have both cases covered 
+
+    -- notes on wolfram alpha 
