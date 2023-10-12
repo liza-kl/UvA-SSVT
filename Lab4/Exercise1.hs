659c5d0989693aeb0d43100a91c23191111552a1
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-10-12 11:23:27 +0200

    :construction: Added stuff to Ex 1 / 2 (not finished)

--- Lab4/Exercise1.hs
+++ Lab4/Exercise1.hs
@@ -1,1 +1,51 @@
-module Exercise1 where
+module Denis.Exercise1 where
+import Test.QuickCheck
+import Data.List
+import System.Random
+import SetOrd
+
+-- Time estimate 30 mins. (Thanks syntax errors... 🙃)
+
+randomInt:: Gen Int
+randomInt = arbitrary
+
+-- 16807 also possible
+randomLCGInt :: Gen Int
+randomLCGInt = do
+    -- (seed * multiplier) % modulus 
+    -- Alternative lel
+    -- currentTime <- getCurrentTime
+    -- let seed = round (utctDayTime currentTime)
+    let nextSeed = (1256785 * 48271) `rem` 2147483647
+    return nextSeed
+
+generateRandomList:: Int -> Gen [Int]
+generateRandomList 0 = pure []
+generateRandomList size = do
+            randInt <- randomInt
+            rest <- generateRandomList (size - 1)
+            return (randInt : rest)
+
+generateRandomSet:: Int -> Gen (Set Int)
+generateRandomSet size = do
+                randomList <- generateRandomList size
+                return (list2set randomList)
+
+generateSets :: Gen (Set Int)
+generateSets = do
+            randInt <- randomInt
+            generateRandomSet randInt
+
+generateRandomList':: Gen [Int]
+generateRandomList' = arbitrary
+
+generateSets':: Gen (Set Int)
+generateSets' = do
+                list2set <$> generateRandomList'
+
+main :: IO ()
+main = do
+    value1 <- generate generateSets
+    value2 <- generate generateSets'
+    print value1
+    print value2
--- Lab4/Exercise2.hs
+++ Lab4/Exercise2.hs
@@ -1,1 +1,81 @@
 module Exercise2 where
+import Test.QuickCheck
+import Data.List
+import System.Random
+import SetOrd
+import Exercise1
+
+-- ## Deliverables
+-- implementations, test properties, short test report, indication of time spent.
+
+-- ## Indication of time spent 80 minutes 
+setUnion :: Ord a => Set a -> Set a -> Set a
+setUnion set1 set2 = unionSet set1 set2
+
+-- A∪B=B∪A
+prop_unionCommulative :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_unionCommulative set1 set2 = property (setUnion set1 set2 == setUnion set2 set1) 
+
+-- (A∪B)∪C=A∪(B∪C)
+prop_unionAssociative :: (Eq a, Ord a) => Set a -> Set a -> Set a -> Property
+prop_unionAssociative set1 set2 set3 = property (setUnion (setUnion set1 set2) set3 == setUnion set1 (setUnion set2 set3))
+
+-- A∪A=A
+prop_unionIdempotent :: (Eq a, Ord a) => Set a -> Property
+prop_unionIdempotent set1 = property (setUnion set1 set1 == set1)
+
+-- A - B
+-- if A is empty, A - B is empty
+-- if B is empty, A - B is A
+-- if none is empty, check if first element of A is in B
+-- if it is, then it shouldn't be included in A - B
+-- so call function recursively with the rest elements as A set
+-- if it is not, it should be added to the result and then
+-- call fuctnion recursively with the rest elements as A set
+setDifference :: Ord a => Set a -> Set a -> Set a
+setDifference (Set []) _ = emptySet
+setDifference set1 (Set []) = set1
+setDifference (Set (x:xs)) set2
+  | inSet x set2 = setDifference (Set xs) set2
+  | otherwise = insertSet x (setDifference (Set xs) set2)
+
+prop_differenceEmptySet1 :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_differenceEmptySet1 set1 set2 = property (setDifference set1 set2 == emptySet)
+
+prop_differenceEmptySet2 :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_differenceEmptySet2 set1 set2 = property (setDifference set1 set2 == set1)
+
+prop_differenceSameSet :: (Eq a, Ord a) => Set a -> Property
+prop_differenceSameSet set1 = property (setDifference set1 set1 == emptySet)
+
+-- A ∩ B
+-- if A or B is empty, A ∩ B is empty
+-- if none is empty, if first element of A is in B, add it to the result
+-- so call function recursively with the rest elements as A set
+-- otherwise, it should not be inclued in A ∩ B, 
+-- so call function recursively with the rest elements as A set
+setIntersection :: Ord a => Set a -> Set a -> Set a
+setIntersection (Set []) _ = emptySet
+setIntersection _ (Set []) = emptySet
+setIntersection (Set (x:xs)) set2
+  | inSet x set2 = insertSet x (setIntersection (Set xs) set2)
+  | otherwise = setIntersection (Set xs) set2
+
+-- A∩B=B∩A
+prop_intersectionCommulative :: (Eq a, Ord a) => Set a -> Set a -> Property
+prop_intersectionCommulative set1 set2 = property (setIntersection set1 set2 == setIntersection set2 set1) 
+
+-- (A∩B)∩C=A∩(B∩C)
+prop_intersectionAssociative :: (Eq a, Ord a) => Set a -> Set a -> Set a -> Property
+prop_intersectionAssociative set1 set2 set3 = property (setIntersection (setIntersection set1 set2) set3 == setIntersection set1 (setIntersection set2 set3))
+
+-- A∩(B∩C)=(A∩B)∪(A∩C)
+prop_intersectionDistributive :: (Eq a, Ord a) => Set a -> Set a -> Set a -> Property
+prop_intersectionDistributive set1 set2 set3 = property (setIntersection set1 (setIntersection set2 set3) == setUnion (setIntersection set1 set2) (setIntersection set1 set3))
+
+-- ## Testing 
+main :: IO()
+main = 
+    putStrLn "Tests using custom generator"
+    putStrLn "Tests using quickCheck generator"
+    quickCheckResult $ forAll generateSets' prop_differenceEmptySet1
