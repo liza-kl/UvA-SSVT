2f841d70d907ce6e411e50774dd55906807579c5
D45Hub <denis.thiessen@gmx.de>
2023-10-18 17:10:44 +0200

    Added parser explanation

--- Lab4/Exercise8.hs
+++ Lab4/Exercise8.hs
@@ -12,18 +12,44 @@ import System.IO
 
 -- Time Spent: 90 minutes
 
+{-
+
+This exercise aims to write a parser for a simple, imaginary, imperative programming language.
+
+Generally, a parser is made up of the following parts:
+
+	- First, we do a lexical analysis of the given string of program code, which yields a list of tokens as the final result.
+	- Then, we construct an abstract syntax tree using this list of tokens.
+	- Using this tree, we can derive the different types of statements and which tokens, together, form, for example, an assignment, a condition or a while loop. Thus creating a list of statements from the given string of program code.
+	
+The lexical analyser uses pre-defined keywords, operators and separators, and self-chosen identifiers in special locations and looks for these kinds of values. (For example, by using regular expressions.)
+Everything else could then be considered as a comment, for example.
+Then, these values or composed values, which satisfy a particular rule, coming from a pre-defined grammar, are put into so-called Tokens.
+This is done for the entire string, which results in a list of tokens. (* these things could also be modified by mutation testing to check up on aspects like lexical analysis and AST generation.)
+
+From this list of tokens, we can derive a tree. A parent node from a tree contains the general syntactic structure, while its children have further information.
+For example, Considering a while loop and how that would be constructed, this could be a parent "while" node, with its condition and body as its child nodes.
+The condition and body could be further broken down like this until we arrive at the primary statements, identifiers, operators, etc.
+
+Finally, when we apply this general tree construction process, we derive this abstract syntax tree from the given list of tokens.
+
+Using this tree, we can derive the order of the statements and also how each statement is constructed from smaller sub-statements.
+
+-}
+
+-- Main statement parser
 stmtParser :: Parser Statement
 stmtParser = do
   statement <- choice [assParser, condParser, sequenceParser, whileParser]
   optional spaces
   return statement
 
+-- Sub-Statement parsers
 assParser :: Parser Statement
 assParser = do
   var <- many1 letter
   spaces >> char '=' >> spaces
-  expr <- exprParser
-  return (Ass var expr)
+  Ass var <$> exprParser
 
 condParser :: Parser Statement
 condParser = do
@@ -32,8 +58,7 @@ condParser = do
   spaces >> string "then" >> spaces
   stmt1 <- stmtParser
   spaces >> string "else" >> spaces
-  stmt2 <- stmtParser
-  return (Cond condition stmt1 stmt2)
+  Cond condition stmt1 <$> stmtParser
 
 sequenceParser :: Parser Statement
 sequenceParser = do
@@ -45,17 +70,16 @@ whileParser = do
   string "while"
   condition <- spaces >> conditionParser
   spaces >> string "do" >> spaces
-  stmt <- stmtParser
-  return (While condition stmt)
+  While condition <$> stmtParser
+
+varParser :: Parser Expr
+varParser = V <$> many1 letter
 
 exprParser :: Parser Expr
 exprParser = choice [numParser, varParser, operationsParser]
 
 numParser :: Parser Expr
-numParser = I <$> read <$> many1 digit
-
-varParser :: Parser Expr
-varParser = V <$> many1 letter
+numParser = I . read <$> many1 digit
 
 operationsParser :: Parser Expr
 operationsParser = do
@@ -79,33 +103,6 @@ conditionParser = choice
   , djParser
   ]
 
-equalityParser :: Parser Condition
-equalityParser = do
-  e1 <- exprParser
-  spaces >> char '=' >> spaces
-  e2 <- exprParser
-  return (Eq e1 e2)
-
-lessThanParser :: Parser Condition
-lessThanParser = do
-  e1 <- exprParser
-  spaces >> char '<' >> spaces
-  e2 <- exprParser
-  return (Lt e1 e2)
-
-greaterThanParser :: Parser Condition
-greaterThanParser = do
-  e1 <- exprParser
-  spaces >> char '>' >> spaces
-  e2 <- exprParser
-  return (Gt e1 e2)
-
-ngParser :: Parser Condition
-ngParser = do
-  string "not" >> spaces
-  c <- conditionParser
-  return (Ng c)
-
 cjParser :: Parser Condition
 cjParser = do
   string "and" >> spaces
@@ -118,6 +115,30 @@ djParser = do
   conditions <- sepBy1 conditionParser (spaces >> string "or" >> spaces)
   return (Dj conditions)
 
+greaterThanParser :: Parser Condition
+greaterThanParser = do
+  e1 <- exprParser
+  spaces >> char '>' >> spaces
+  Gt e1 <$> exprParser
+
+ngParser :: Parser Condition
+ngParser = do
+  string "not" >> spaces
+  Ng <$> conditionParser
+
+equalityParser :: Parser Condition
+equalityParser = do
+  e1 <- exprParser
+  spaces >> char '=' >> spaces
+  Eq e1 <$> exprParser
+
+lessThanParser :: Parser Condition
+lessThanParser = do
+  e1 <- exprParser
+  spaces >> char '<' >> spaces
+  Lt e1 <$> exprParser
+
+-- Declarations for custom read and show functions
 customRead :: String -> Either ParseError Statement
 customRead = parse stmtParser ""
 
@@ -152,7 +173,7 @@ main = do
 
 -- tokenize statements string based on semicolon delimeter
 splitStatements :: String -> [String]
-splitStatements input = splitOn ";" input
+splitStatements = splitOn ";"
 
 -- parse each statement
 -- for some reason, it is stuck after the first parsed statement
@@ -165,6 +186,6 @@ iterateStatements (token:rest) = do
     Left err -> putStrLn $ "Parse error: " ++ show err
     Right statement -> customShow statement
   putStrLn "Press Enter to continue"
-  _ <- hFlush stdout  
+  _ <- hFlush stdout
   _ <- getLine
   iterateStatements rest
